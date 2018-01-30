0b21c01346171e9102189858bb2ea5404cb097e0
Navin prasad <navinprasad0808@gmail.com>
2018-01-30 18:33:12 +0530

    Movie API Project completed

--- API/Movie/server/movie.js
+++ API/Movie/server/movie.js
@@ -5,8 +5,16 @@ var request = require('request');
 // Default engine and extension
 app.set('view engine', 'ejs');
 
+app.get("/", function(req, res){
+  res.render("search");
+});
+
 app.get("/results", function(req, res){
-  request("http://www.omdbapi.com/?s=india&apikey=7b879098", function(error, response, body){
+  // Getting the search input  from '/' route and stored it in 'query' variable
+  var query = req.query.movie;
+  var url = 'http://www.omdbapi.com/?s=' +query+ '&apikey=7b879098';
+
+  request(url, function(error, response, body){
     if(!error && response.statusCode == 200){
       var data = JSON.parse(body);
       // res.send(data['Search']);
@@ -16,5 +24,5 @@ app.get("/results", function(req, res){
 });
 
 app.listen(3000, '127.0.0.1', function(){
-  console.log("Movie App has started")
+  console.log("Movie App has started on 3000 port")
 });
